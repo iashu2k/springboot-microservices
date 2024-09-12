10678eacab756a481e988dd4888c913ce6b486e1
Ashutosh Mishra <ashu@Ashutoshs-MacBook-Air.local>
2024-09-12 11:48:14 +0530

    initial commit

---
+++ companyms/src/main/java/com/embarkx/companyms/company/CompanyService.java
@@ -0,0 +1,15 @@
+package com.embarkx.companyms.company;
+
+import com.embarkx.companyms.company.dto.ReviewMessage;
+
+import java.util.List;
+
+public interface CompanyService {
+    List<Company> getAllCompanies();
+    boolean updateCompany(Company company, Long id);
+    void createCompany(Company company);
+    boolean deleteCompanyById(Long id);
+    Company getCompanyById(Long id);
+
+    void updateCompanyRating(ReviewMessage reviewMessage);
+}
---
+++ jobms/src/main/java/com/embarkx/jobms/job/impl/JobServiceImpl.java
@@ -0,0 +1,104 @@
+package com.embarkx.jobms.job.impl;
+
+import com.embarkx.jobms.job.Job;
+import com.embarkx.jobms.job.JobRepository;
+import com.embarkx.jobms.job.JobService;
+import com.embarkx.jobms.job.clients.CompanyClient;
+import com.embarkx.jobms.job.clients.ReviewClient;
+import com.embarkx.jobms.job.dto.JobDTO;
+import com.embarkx.jobms.job.external.Company;
+import com.embarkx.jobms.job.external.Review;
+import com.embarkx.jobms.job.mapper.JobMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class JobServiceImpl implements JobService {
+    // private List<Job> jobs = new ArrayList<>();
+    JobRepository jobRepository;
+    private CompanyClient companyClient;
+    private ReviewClient reviewClient;
+
+    @Autowired // asking spring to give an instance of restTemplate at runtime since this is a bean
+    RestTemplate restTemplate;
+
+    public JobServiceImpl(JobRepository jobRepository, CompanyClient companyClient, ReviewClient reviewClient) {
+        this.companyClient = companyClient;
+        this.reviewClient = reviewClient;
+        this.jobRepository = jobRepository;
+    }
+
+    @Override
+//    @CircuitBreaker(name="companyBreaker", fallbackMethod = "companyBreakerFallback")
+//    @Retry(name="companyBreaker", fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name="companyBreaker", fallbackMethod = "companyBreakerFallback")
+    public List<JobDTO> findAll() {
+        List<Job> jobs = jobRepository.findAll();
+        List<JobDTO> jobDTOS = new ArrayList<>();
+
+        return jobs.stream().map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> companyBreakerFallback(Exception e){
+        List<String> list = new ArrayList<>();
+        list.add("Dummy");
+        return list;
+    }
+
+    private JobDTO convertToDto(Job job) {
+        Company company = companyClient.getCompany(job.getCompanyId());
+        List<Review> reviews = reviewClient.getReviews(job.getCompanyId());
+        return JobMapper.maptoJobWithCompanyDto(job, company, reviews);
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobRepository.save(job);
+    }
+
+    @Override
+    public JobDTO getJobById(Long id) {
+        Job job = jobRepository.findById(id).orElse(null);
+        return convertToDto(job);
+
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if (jobOptional.isPresent()) {
+            Job job = jobOptional.get();
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setMinSalary(updatedJob.getMinSalary());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setLocation(updatedJob.getLocation());
+            jobRepository.save(job);
+            return true;
+        }
+        return false;
+    }
+}
